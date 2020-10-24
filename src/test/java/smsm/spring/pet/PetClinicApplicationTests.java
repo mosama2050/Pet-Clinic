ab516fe9205457fbf4c0046339cd67ad0b86d63e
mohama25 <immardoo@gmail.com>
2020-10-24 10:27:35 +0200

    first commit

---
+++ src/test/java/smsm/spring/pet/PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package smsm.spring.pet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
