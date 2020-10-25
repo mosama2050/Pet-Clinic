7c59626427b1b0960a40d88f24bd9beb9cbb1bf2
mohama25 <immardoo@gmail.com>
2020-10-25 16:08:24 +0200

    adding POJOS. closes#2.

---
+++ src/main/java/smsm/spring/pet/model/Owner.java
@@ -0,0 +1,4 @@
+package smsm.spring.pet.model;
+
+public class Owner extends Person{
+}
---
+++ src/main/java/smsm/spring/pet/model/petType.java
@@ -0,0 +1,13 @@
+package smsm.spring.pet.model;
+
+public class petType {
+private  String name ;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
