0903261f2f5810ff384f7c63cd1575be6e237f2c
mohama25 <immardoo@gmail.com>
2020-11-08 03:24:37 +0200

    id auto generation.   closes #17

--- pet-clinic-data/src/main/java/smsm/spring/pet/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/smsm/spring/pet/services/map/AbstractMapService.java
@@ -1,12 +1,12 @@
 package smsm.spring.pet.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import smsm.spring.pet.model.BaseEntity;
 
-public abstract class AbstractMapService <T,ID> {
-    protected Map<ID,T> map = new HashMap<>();
+import java.util.*;
+
+public abstract class AbstractMapService <T extends BaseEntity,ID extends Long> {
+    protected Map<Long,T> map = new HashMap<>();
+
     Set<T> findAll (){
         return  new HashSet<>(map.values());
     }
@@ -15,8 +15,16 @@ public abstract class AbstractMapService <T,ID> {
         return  map.get(id);
     }
 
-    T save (ID id ,T t) {
-        map.put(id ,t );
+    T save  (T t) {
+      if(t != null){
+          if(t.getId() == null){
+              t.setId(getNextId());
+          }
+          map.put(t.getId() , t);
+      }else{
+          throw  new RuntimeException("Null Object");
+
+      }
         return  t;
     }
 
@@ -26,5 +34,17 @@ public abstract class AbstractMapService <T,ID> {
 
     void delete(T t){
         map.entrySet().removeIf(idtEntry ->idtEntry.getValue().equals(t) );
+    }
+
+    private  Long getNextId(){
+        Long nextId =null;
+      try {
+          nextId =Collections.max(map.keySet())+1;
+      }catch (NoSuchElementException e){
+        nextId = 1L;
+
+      }
+        return nextId;
+
     }
 }
